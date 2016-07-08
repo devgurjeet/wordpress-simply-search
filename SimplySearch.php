d898fd1454f4f5b97627c0102a6fafc98f3cbef2
Gurjeet Singh <developer.gurjeet@gmail.com>
2016-07-08 14:48:51 +0530

    admin section complete

--- SimplySearch.php
+++ SimplySearch.php
@@ -15,6 +15,9 @@ class SimplySearch {
     //** Constructor **//
     function __construct() {
 
+        add_action( 'admin_enqueue_scripts',  array(&$this, 'loadAssectCss') );
+        // add_action( 'admin_enqueue_scripts',  array(&$this, 'loadAdminAssects') );
+
         //** Register menu. **//
         add_action('admin_menu', array(&$this, 'register_plugin_menu') );
     }
@@ -24,11 +27,50 @@ class SimplySearch {
             add_menu_page( 'Simply Search', 'Simply Search', 'manage_options', 'simplysearch', array(&$this, 'admin_page'), 'dashicons-search', 26 );
     }
 
+    function loadAssectCss( ){     
+      
+        //** Load  Styling. **//        
+       wp_enqueue_style( 'simply-search-style-css', plugins_url( '/css/style.css', __FILE__ ) );
+
+    }
+
+
 
     /*function to show the page. */
     function admin_page(){
-        /*Admin page content goes here */
-        echo "<h1>Simply Search Settings</h1>";
+
+        if(isset($_POST['ss_save_form'])){
+            
+            $option_value = trim($_POST['post_block_section']);
+            $result       = update_option( 'ss_post_filter', $option_value, '', 'yes' );
+
+            if($result){
+                echo     '<h1 class="success_message">Setting saved successfully.</h1>';                
+            }else{              
+                echo     '<h1 class="error_message">Setting not saved.</h1>';                
+            }
+            
+
+        }
+        
+        ?> 
+        <div class="ss_cotainer">      
+            <form action="" method="POST" class="ss_form">      
+                
+                <h1 class="ss_h1">Simply Search Settings</h1>
+                
+                <fieldset class="ss_fieldset">
+                    <!-- <legend><span class="number">2</span>Your profile</legend> -->
+                    <label class="ss_label" for="post_block_section"><strong>Post Types</strong></label>
+                    <textarea class="ss_textarea" id="post_block_section" name="post_block_section"><?php echo get_option( 'ss_post_filter',true);?> </textarea>
+                    <p class="ss_notice"><strong>Note:</strong> You can add multiple post types seperated by comma. example: post, page</p>
+                </fieldset>
+                <input type="hidden" name="ss_save_form" value="true" />
+                <button class="ss_button" type="submit">Save Settings</button>
+            </form>
+        </div>
+        <?php     
+        
     }
 }
 
